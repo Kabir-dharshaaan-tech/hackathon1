a83cd622e2fa07cb23e075f6a3406d53be8d56a5
kabir-dharshaaan-tech <dharshaankabir@gmail.com>
2025-03-29 21:05:00 +0530

    done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const UserForm = require("../models/UserForm");
-const authMiddleware = require("../middleware/auth"); 
+const authMiddleware = require("../middleware/authMiddleware"); 
 
 const router = express.Router();
 
